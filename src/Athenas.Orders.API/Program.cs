c57d06613d84da4fd2878c315f78960da69dc83d
ebranquinho <branquinhomanu@gmail.com>
2025-10-25 17:23:17 -0300

    add endpoints to create contract and order

--- src/Athenas.Orders.API/Program.cs
+++ src/Athenas.Orders.API/Program.cs
@@ -14,29 +14,30 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapPost("/api/v1/contract", async (
+    [FromServices] IContractService service,
+    [FromBody] CreateContractDto dto,
+    CancellationToken cancellationToken
+    ) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var contract = await service.CreateAsync(dto, cancellationToken);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return Results.Created("api/v1/contract", new { Id = contract.Id, Number = contract.Number });
 })
-.WithName("GetWeatherForecast")
+.WithName("CreateOrder")
 .WithOpenApi();
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+app.MapPost("/api/v1/orders", async (
+    [FromServices] IOrderService service,
+    [FromBody] CreateOrderDto dto,
+    CancellationToken cancellationToken
+    ) =>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    await service.CreateAsync(dto, cancellationToken);
+
+    return Results.Created();
+})
+.WithName("CreateOrder")
+.WithOpenApi();
+
+await app.RunAsync();
--- src/Athenas.Orders.API/Usings.cs
+++ src/Athenas.Orders.API/Usings.cs
@@ -1,1 +1,4 @@
 global using Athenas.Orders.Infra.Extensions;
+global using Microsoft.AspNetCore.Mvc;
+global using Athenas.Orders.Application.Dtos;
+global using Athenas.Orders.Application.Services.Interfaces;
